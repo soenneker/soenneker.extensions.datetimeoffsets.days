4deaf328f0d19d561ed5189267776da81bb43f9c
Jake Soenneker <jake@soenneker.com>
2026-01-02 18:23:05 -0600

    Initial commit

---
+++ test/Soenneker.Extensions.DateTimeOffsets.Days.Tests/DateTimeOffsetsDayExtensionTests.cs
@@ -0,0 +1,13 @@
+﻿using Soenneker.Tests.Unit;
+using Xunit;
+
+namespace Soenneker.Extensions.DateTimeOffsets.Days.Tests;
+
+public sealed class DateTimeOffsetsDayExtensionTests : UnitTest
+{
+    [Fact]
+    public void Default()
+    {
+
+    }
+}
